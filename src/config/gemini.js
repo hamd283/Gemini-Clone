a538530a13a9ac395ebb2a05dabc4c8d4ce6dd27
Hamd283 <hamdbhatti283@gmail.com>
2024-07-30 13:19:29 +0500

    gemini clone

---
+++ src/config/gemini.js
@@ -0,0 +1,42 @@
+/*
+ * Install the Generative AI SDK
+ *
+ * $ npm install @google/generative-ai
+ *
+ * See the getting started guide for more information
+ * https://ai.google.dev/gemini-api/docs/get-started/node
+ */
+
+import { GoogleGenerativeAI, HarmCategory, HarmBlockThreshold } from "@google/generative-ai";
+
+const api_Key ="AIzaSyCZe2gQax_BMBHFGQMmViKThz_A9Kde3vs";
+const genAI = new GoogleGenerativeAI(api_Key);
+
+const model = genAI.getGenerativeModel({
+  model: "gemini-1.5-flash",
+});
+
+const generationConfig = {
+  temperature: 1,
+  topP: 0.95,
+  topK: 64,
+  maxOutputTokens: 8192,
+  responseMimeType: "text/plain",
+};
+
+async function run(prompt) {
+  const chatSession = model.startChat({
+    generationConfig,
+ // safetySettings: Adjust safety settings
+ // See https://ai.google.dev/gemini-api/docs/safety-settings
+    history: [
+    ],
+  });
+
+  const result = await chatSession.sendMessage(prompt);
+  const response= result.response;
+  console.log(response.text());
+  return (response.text());
+}
+
+export default run;
